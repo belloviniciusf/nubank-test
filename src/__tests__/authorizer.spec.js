d14ad277e46bbb74f1d4f8488d8683306260c929
Vinícius Belló <bello.viniciusf@gmail.com>
2021-09-15 22:12:19 -0300

    create setup from readline interface

--- src/__tests__/authorizer.spec.js
+++ src/__tests__/authorizer.spec.js
@@ -9,6 +9,20 @@ const readline = require('readline');
 jest.mock('../models/Account');
 jest.mock('readline');
 
+const setupReadlineInterface = (commands) => {
+    readline.createInterface = jest.fn().mockReturnValue({
+        addListener: jest.fn()                                        
+            .mockImplementationOnce((_, cb) => {                        
+                commands.map((command) => {
+                    cb(JSON.stringify(command))
+                })                        
+            })                                        
+            .mockImplementationOnce((_, cb) => {
+                cb()
+            })                        
+    });    
+};
+
 describe('authorizer', () => {    
     describe('receiveMessage', () => {
         it('returns a message of "account" type', () => {                
@@ -111,26 +125,21 @@ describe('authorizer', () => {
     describe('start', () => {
         beforeEach(() => {
             Account.mockClear();            
+            console.log = jest.fn();
         })
 
-        it('reads command and then log', () => {               
-            console.log = jest.fn();            
+        it('reads command and then log', () => {                           
+            const commands = [
+                { account: {} }
+            ]
 
             Account.mockImplementation(() => {
                 return {                                         
                     getLogMessage: () => MOCK_ACCOUNT_DATA.account
                 }                                
             });
-            
-            readline.createInterface = jest.fn().mockReturnValue({
-                addListener: jest.fn()                                        
-                    .mockImplementationOnce((_, cb) => {                        
-                        cb('{ "account": {} }')
-                    })
-                    .mockImplementationOnce((_, cb) => {
-                        cb()
-                    })                        
-            })            
+
+            setupReadlineInterface(commands);                    
 
             start();
 
@@ -143,14 +152,19 @@ describe('authorizer', () => {
         });
 
         it('returns "account-already-initialized" violation when account already exists', () => {
-            console.log = jest.fn();
+            const commands = [
+                {"account": {"active-card": true, "available-limit": 175}},
+                {"account": {"active-card": true, "available-limit": 350}}                   
+            ];
 
             Account
-                .mockImplementationOnce(() => { return { getLogMessage: () => MOCK_ACCOUNT_DATA.account } })
+                .mockImplementationOnce(() => { 
+                    return { getLogMessage: () => MOCK_ACCOUNT_DATA.account } 
+                })
                 .mockImplementationOnce(() => { return { getLogMessage: () => ({
                     ...MOCK_ACCOUNT_DATA.account,
                     'available-limit': 350
-                })}});                    
+                })}});
 
             const mockGetInstance = jest.fn()
                 .mockReturnValueOnce()
@@ -160,16 +174,7 @@ describe('authorizer', () => {
             
             Account.getInstance = mockGetInstance;            
 
-            readline.createInterface = jest.fn().mockReturnValue({
-                addListener: jest.fn()                                        
-                    .mockImplementationOnce((_, cb) => {                        
-                        cb('{"account": {"active-card": true, "available-limit": 175}}')
-                        cb('{"account": {"active-card": true, "available-limit": 350}}')
-                    })                                        
-                    .mockImplementationOnce((_, cb) => {
-                        cb()
-                    })                        
-            });                        
+            setupReadlineInterface(commands);            
 
             start();                     
 
